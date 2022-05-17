90f0029f16d349697540adf56553be8b8f0d42de
Michał Wisełka <mwiselka@p24.int>
2022-05-17 11:50:51 +0200

    Added graphs to subplots

--- test.py
+++ test.py
@@ -9,7 +9,9 @@ FirstClass = a.data1.number_of_surviors_in_1stclass
 SecondClass = a.data1.number_of_surviors_in_2ndclass
 ThirdClass = a.data1.number_of_surviors_in_3rdclass
 
+#Implemnt this in some time -> fig.savefig('out.png', dpi=100)
 
+# Test graph
 """
 fig1 = plt.figure(1)
 x = [1,2,3]
@@ -27,39 +29,35 @@ plt.xlabel('x - axis')
 plt.ylabel('y - axis')
 """
 
-fig3 = plt.figure(3)
+#fig3 = plt.figure(3)
 
 left = [1, 2, 3]
-
-
 height = [FirstClass, SecondClass, ThirdClass]
+tick_label = ['1stClass', '2ndClass', '3rdClass']
 
-
-tick_label = ['FirstClass', 'Second Class', 'ThirdClass']
-
-
+"""
 plt.bar(left, height, tick_label=tick_label,
-        width=0.8, color=['red', 'green'])
+        width=0.8, color=['#99e6ff', '#0086b3', '#66d9ff'])
 
 
-plt.xlabel('x - axis')
+plt.xlabel('Class')
 
-plt.ylabel('y - axis')
-plt.title('My bar chart!')
+plt.ylabel('Number of Surviors')
+plt.title('Number of Surviors based on passenger class')
+"""
 
-plt.show()
-
-
-fig2 = plt.figure(2)
+#fig2 = plt.figure(2)
 
 activities = ['female', 'male']
 slices = [female, male]
 colors = ['#ff6666', '#00bfff']
 
+"""
 plt.title("Chances of survival")
 plt.pie(slices, labels=activities, colors=colors,
         startangle=90, shadow=True, explode=(0.02, 0.02),
         radius=1.2, autopct='%1.1f%%')
+"""
 
 
 # plt.legend()
@@ -69,5 +67,23 @@ ax1.pie(slices, labels=activities, colors=colors,
         startangle=90, shadow=True, explode=(0.02, 0.02),
         radius=1.2, autopct='%1.1f%%')
 ax1.set_title('Chances of survival, based on sex')
-ax2.plot(x, y, label="pierwsza")
+ax2.bar(left, height, tick_label=tick_label,
+        width=0.8, color=['#99e6ff', '#0086b3', '#66d9ff'])
+ax2.set_xlabel('Class')
+ax2.set_ylabel('Number of Surviors')
+ax2.set_title('Number of Surviors based on passenger class')
+ax2.legend(['First class','Second class', 'Third class'])
+
+
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.9, 
+                    hspace=0.9)
+
+#fig.tight_layout()
+fig.set_size_inches(8.5,6)
+fig.savefig('testpng', dpi=100)
 plt.show()
